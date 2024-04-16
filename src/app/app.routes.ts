eb7135eac82a0eb80e01ffa5075c62bc94d51b9e
sanflor <902olbalarconsantiago@gmail.com>
2024-04-16 10:57:42 -0500

    creacion de formulario para crear un nuevo contacto y funcionalidad de botones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,10 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./contact-list/contact-list.component')
     
+    },
+    {
+        path: 'new',
+        loadComponent: () => import('./contact-form/contact-form.component')
+    
     }
 ];
---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.css'
+})
+export default class ContactFormComponent {
+
+}

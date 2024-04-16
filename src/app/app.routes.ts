c4e5d4b5bb8a8ee4db25a23fe4050cbbdba36528
sanflor <902olbalarconsantiago@gmail.com>
2024-04-16 10:16:15 -0500

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: '',
+        loadComponent: () => import('./contact-list/contact-list.component')
+    
+    }
+];
---
+++ src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { ContactService } from '../services/contact.service';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-contact-list',
+  standalone: true,
+  imports: [DatePipe],
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css'
+})
+export default class ContactListComponent implements OnInit {
+  private contactService = inject(ContactService)
+
+  contacts: any[] = [];
+
+  ngOnInit(): void {
+      this.contactService.list().subscribe((contacts: any) => {
+        this.contacts = contacts;
+      });
+
+  }
+
+}
